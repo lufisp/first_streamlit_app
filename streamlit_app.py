a0fc49843743cfc2fd87c202081d5eefce97369e
lpereira <luiz.pereira@revelate.co>
2023-08-20 11:24:13 -0400

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard Boiled Eggs & Avocado Toast');
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie');
+streamlit.text('🐔 Hard Boiled Eggs')
+streamlit.txt('🥑🍞Avocado Toast');
